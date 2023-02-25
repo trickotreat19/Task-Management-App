3cfe19c7b846e682047758786fab46dfa347d038
trickotreat19 <prajwalray@yahoo.in>
2023-02-25 23:33:59 +0530

    3rd

--- lib/widgets/dailyListView.dart
+++ lib/widgets/dailyListView.dart
@@ -3,37 +3,38 @@ import 'package:flutter/material.dart';
 import 'package:task_management/models/tasks.dart';
 
 class DailyListView extends StatelessWidget {
-  const DailyListView(this.dailyListView,{super.key});
+  const DailyListView(this.dailyListView, {super.key});
   final List<Tasks> dailyListView;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: 
-         ListView.builder(
-          itemCount: dailyListView.length,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context,index){
-            return Card(
-              elevation: 7,
-              child: ListTile(
-                title: AutoSizeText(dailyListView[index].title, style: const TextStyle(fontSize: 20),
-                minFontSize: 16,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                ),
-                        subtitle: AutoSizeText(dailyListView[index].subTasks.toString(),style: const TextStyle(fontSize: 18),
-                minFontSize: 10,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,),
-                        
-              ),
-            );
-          
-         })
-          
-        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: dailyListView.length,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 7,
+                    child: ListTile(
+                      title: AutoSizeText(
+                        dailyListView[index].title,
+                        style: const TextStyle(fontSize: 20),
+                        minFontSize: 16,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: AutoSizeText(
+                        dailyListView[index].subTasks.toString(),
+                        style: const TextStyle(fontSize: 18),
+                        minFontSize: 10,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  );
+                })),
       ],
     );
   }
